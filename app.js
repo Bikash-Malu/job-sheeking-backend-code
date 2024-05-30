98b28205fb231abc8b469f9f1d28dc2257548ecb
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-05-30 21:45:39 +0530

    change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ['https://job-sheeking-frotend-code.vercel.app/'],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
